e567e86cdc4ee0aa061504d3f9d83d48a2007f9e
巴哈姆特 <ttwings@126.com>
2018-10-04 01:43:06 +0800

    suit test. use suit lib,create gui.

--- src/objects/StageShop.lua
+++ src/objects/StageShop.lua
@@ -8,6 +8,8 @@
 --- @field world World
 --- @field area Area
 
+local suit = require("lib.suit")
+
 StageShop = Object:extend()
 
 function StageShop:new()
@@ -35,8 +37,31 @@ function StageShop:new()
 
 end
 
+local input = {text = ""}
+
 function StageShop:update(dt)
     if self.area then self.area:update(dt) end
+    --- suit update
+    -- put the layout origin at position (100,100)
+    -- the layout will grow down and to the right from this point
+    suit.layout:reset(100,100)
+
+    -- put an input widget at the layout origin, with a cell size of 200 by 30 pixels
+    suit.Input(input, suit.layout:row(200,30))
+
+    -- put a label that displays the text below the first cell
+    -- the cell size is the same as the last one (200x30 px)
+    -- the label text will be aligned to the left
+    suit.Label("Hello, "..input.text, {align = "left"}, suit.layout:row())
+
+    -- put an empty cell that has the same size as the last cell (200x30 px)
+    suit.layout:row()
+
+    -- put a button of size 200x30 px in the cell below
+    -- if the button is pressed, quit the game
+    if suit.Button("Close", suit.layout:row()).hit then
+        love.event.quit()
+    end
 end
 
 function StageShop:draw()
@@ -48,6 +73,8 @@ function StageShop:draw()
     love.graphics.rectangle("line",1,1,gw/3 -1 ,gh - 2,8)
     love.graphics.rectangle("line",1 + gw/3,1,gw/3 -1,gh - 2,8)
     love.graphics.rectangle("line",1 + gw * 2/3,1,gw/3 - 1,gh - 2,8)
+    ---- suit gui
+    suit.draw()
 
     camera:detach()
     love.graphics.setCanvas()
